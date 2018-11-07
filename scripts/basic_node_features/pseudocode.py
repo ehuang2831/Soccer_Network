07aa81f550daf9669cea59de8e7f301338ce3b93
Sandeep Chinchali <csandeep@stanford.edu>
2018-11-07 13:23:58 -0800

    update

--- scripts/basic_node_features/pseudocode.py
+++ scripts/basic_node_features/pseudocode.py
@@ -24,7 +24,6 @@ def get_single_player_features(match_id = None, player_id = None):
 """
 
 def get_match_aggregated_feature_vectors(match_id = None, home_team_id = None, away_team_id = None):
-
     z_u_list = []
     for player_u in home_team:
         
@@ -40,6 +39,50 @@ def get_match_aggregated_feature_vectors(match_id = None, home_team_id = None, a
 
     concat_match_feature_vector = [home_team_average, away_team_average]    
 
-    return concat_match_feature_vector
+    return home_team_average, away_team_average, concat_match_feature_vector
 
+"""
+    cluster the team feature vectors using T-SNE or K-MEANS
+
+    color by win, loss or cluster_id
+
+    can also cluster concat_match_feature_vector
+
+    generate 
+    FEATURE_VECTOR_DICT = {}
+
+    keys: match_id
+
+    values: list of dicts
+        v[0] = [home_team_id, away_team_id] 
+
+        v[1] = [home_team_average, away_team_average]
+
+        v[2] = [home_team_sum, away_team_sum]
+        
+        v[3] = z_u_list = [(player_u_id, z_u), ...] [HOME player feature vectors]
+        
+        v[4] = z_v_list = [(player_v_id, z_v), ...] [AWAY player feature vectors]
+
+
+
+"""
+
+def cluster_team_feature_vectors(all_match_id_list = None)
+
+    vectors_to_cluster = []
+
+    feature_vector_dict = {}
+
+    for match_id in all_match_id_list:
+
+        concat_match_feature_vector = get_match_aggregated_feature_vectors(match_id = None, home_team_id = None, away_team_id = None)
+
+        # add to feature_vector_dict
+
+        vectors_to_cluster.append(concat_match_feature_vector)
+
+    STANDARD_CLUSTER_ROUTINE(vectors_to_cluster)
+    
+    visualize_clustering()
 
