ca2d3ee14463cd598451ed00032eca2f7dd30bbb
Sandeep Chinchali <csandeep@stanford.edu>
2018-11-06 16:32:40 -0800

    update

--- scripts/basic_stats/parse_game_csv.py
+++ scripts/basic_stats/parse_game_csv.py
@@ -38,5 +38,17 @@ if __name__ == '__main__':
 
         print 'COLUMNS: ', list(df)
 
+        # COLUMNS:  ['season_id', 'match_id', 'home_team_id', 'home_team_name', 'away_team_id', 'away_team_name', 'id', 'event_id', 'date', 'time', 'period_id', 'min', 'sec', 'team_id', 'player_id', 'playerName', 'playerPosition', 'x', 'y', 'type', 'description', 'outcome']
 
+        columns_of_interest = ['season_id', 'match_id', 'home_team_id', 'home_team_name', 'away_team_id', 'away_team_name', 'id', 'event_id', 'period_id', 'team_id', 'player_id', 'playerName', 'playerPosition', 'type', 'description', 'outcome']
+
+        for column_name in columns_of_interest:
+            unique_set = set(df[column_name])
+
+            print column_name, ' , : ', len(unique_set)
+
+            if len(unique_set) < 10:
+                print column_name, ' , : ', unique_set
+            print ' ' 
+            print '## ' 
 
