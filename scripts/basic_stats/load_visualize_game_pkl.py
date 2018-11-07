753dd9c61175e7becec681cb0e9e074cf9989786
Sandeep Chinchali <csandeep@stanford.edu>
2018-11-07 10:19:50 -0800

    update

--- scripts/basic_stats/load_visualize_game_pkl.py
+++ scripts/basic_stats/load_visualize_game_pkl.py
@@ -43,8 +43,9 @@ if __name__ == '__main__':
 
     league = 'epl'
 
+    # PER PLAYER GOAL DICT
+    ########################################
     per_player_goal_dict_fname = 'results/' + league + '_per_player_goal_dict.pkl'
-    
     per_player_goal_dict = load_pkl(fname = per_player_goal_dict_fname)
 
     num_goals_vec = [v['success_goals'] for k,v in per_player_goal_dict.iteritems()]
@@ -55,3 +56,15 @@ if __name__ == '__main__':
     plot_fname = 'results/' + league + 'per_player_goal_hist.pdf'
 
     plot_histogram(count_vector = num_goals_vec, num_bins = 20, normed = False, xlabel = 'num goals', ylabel = 'Num players', title = 'Goal distro. by player for scorers (at least 1 goal)', plot_fname = plot_fname)
+
+    # MATCH GOAL DICT
+    ########################################
+    match_goal_dict_fname = 'results/' + league + '_match_goal_dict.pkl'
+    match_goal_dict = load_pkl(fname = match_goal_dict_fname)
+
+    # how this looks:
+    # 442366 {'away_team_goal_list': ['Aguero, Sergio', 'Dzeko, Edin'], 'home_team_name': 'Reading', 'home_team_id': 108, 'away_team_name': 'Manchester City', 'team_id_goal_list': [43, 43], 'away_team_id': 43, 'home_team_goal_list': []}
+    # 442367 {'away_team_goal_list': ['Dempsey, Clint', 'Adebayor, Emmanuel'], 'home_team_name': 'Stoke City', 'home_team_id': 110, 'away_team_name': 'Tottenham Hotspur', 'team_id_goal_list': [110, 6, 6], 'away_team_id': 6, 'home_team_goal_list': ["N'Zonzi, Steven"]}
+
+
+
