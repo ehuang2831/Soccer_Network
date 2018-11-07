193f13e4004260d98c6d3bc4e2c25d08c53c5b93
Sandeep Chinchali <csandeep@stanford.edu>
2018-11-06 17:45:05 -0800

    update

--- scripts/basic_stats/load_visualize_game_pkl.py
+++ scripts/basic_stats/load_visualize_game_pkl.py
@@ -27,6 +27,17 @@ GAME_CSV_DIR = DATA_DIR + '/raw_soccer_csv/'
 from basic_snap_utils import *
 from textfile_utils import *
 
+
+def plot_histogram(count_vector = None, num_bins = None, normed = False, xlabel = None, ylabel = None, title = None, plot_fname = None):
+
+    n, bins, patches = plt.hist(count_vector, num_bins, facecolor='green', alpha=0.75, normed = normed)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.savefig(plot_fname)
+    plt.close()
+
+
 if __name__ == '__main__':
     print 'ROOT_SOCCER_DIR: ', ROOT_SOCCER_DIR
 
@@ -36,6 +47,11 @@ if __name__ == '__main__':
     
     per_player_goal_dict = load_pkl(fname = per_player_goal_dict_fname)
 
-    for k,v in per_player_goal_dict.iteritems():
-        print k, v
+    num_goals_vec = [v['success_goals'] for k,v in per_player_goal_dict.iteritems()]
 
+    print 'max goals: ', max(num_goals_vec)
+    print 'min goals: ', min(num_goals_vec)
+
+    plot_fname = 'results/' + league + 'per_player_goal_hist.pdf'
+
+    plot_histogram(count_vector = num_goals_vec, num_bins = 20, normed = False, xlabel = 'num goals', ylabel = 'Num players', title = 'Goal distro. by player for scorers (at least 1 goal)', plot_fname = plot_fname)
