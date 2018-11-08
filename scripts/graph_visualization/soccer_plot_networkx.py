0b52ba4bdfd59ca62dd29a0cfba2e77e96e1cd2a
Sandeep Chinchali <csandeep@stanford.edu>
2018-11-08 13:38:12 -0800

    update

--- scripts/graph_visualization/soccer_plot_networkx.py
+++ scripts/graph_visualization/soccer_plot_networkx.py
@@ -10,7 +10,6 @@ import itertools
 import pandas
 
 from matplotlib.pyplot import cm
-import networkx as nx
 
 # configure paths
 ####################
@@ -30,13 +29,90 @@ GAME_CSV_DIR = DATA_DIR + '/raw_soccer_csv/'
 from basic_snap_utils import *
 from textfile_utils import *
 
-
 league = 'epl'
+
+# MATCH GOAL DICT
+########################################
+match_goal_dict_fname = DATA_DIR + '/' + league + '_match_goal_dict.pkl'
+match_goal_dict = load_pkl(fname = match_goal_dict_fname)
+
 
 # game dictionaries
 ########################################
 simple_game_features_pkl = DATA_DIR + '/simple_features_by_game.pkl'
 simple_game_features_df = load_pkl(fname = simple_game_features_pkl)
+
+# EPL MATCHES DICT
+########################################
+epl_graph_pkl_fname = DATA_DIR + '/EPL_Games.pkl'
+epl_graph_dict = load_pkl(fname = epl_graph_pkl_fname)
+epl_graph_columns = list(epl_graph_dict)
+
+# PER PLAYER GOAL DICT
+########################################
+per_player_goal_dict_fname = DATA_DIR + '/' + league + '_per_player_goal_dict.pkl'
+per_player_goal_dict = load_pkl(fname = per_player_goal_dict_fname)
+
+match_ids = epl_graph_dict.keys()
+
+example_match = match_ids[0]
+
+single_match_info = epl_graph_dict[example_match]
+
+home_time_weight_graph_adj = single_match_info[5]
+
+away_time_weight_graph_adj = single_match_info[6]
+
+nodes_list = []
+
+node_role_dict = {}
+
+edge_list = []
+
+for k, v in home_time_weight_graph_adj.iteritems():
+    
+    player_id_0 = k[0]
+    player_id_1 = k[1]
+
+    if player_id_0 in per_player_goal_dict.keys():
+        player_info = per_player_goal_dict[player_id_0]
+        pname_0 = player_info['playerName'].split(',')[0]
+        player_position = player_info['playerPosition']
+    else:
+        pname_0 = player_id_0
+        player_position = pname_0
+
+    node_role_dict[pname_0] =  player_position
+    nodes_list.append(pname_0)
+
+    if player_id_1 in per_player_goal_dict.keys():
+        player_info = per_player_goal_dict[player_id_1]
+        pname_1 = player_info['playerName'].split(',')[0]
+        player_position = player_info['playerPosition']
+    else:
+        pname_1 = player_id_1
+        player_position = pname_1
+
+
+    node_role_dict[pname_1] =  player_position
+    nodes_list.append(pname_1)
+
+    print (pname_0, pname_1), v
+
+    if v > 0.0:
+        edge_list.append((pname_0, pname_1))
+
+
+unique_nodes = list(set(nodes_list))
+
+print 'unique_nodes: ', unique_nodes
+print ' '
+print 'node roles: ', node_role_dict
+print ' '
+print 'unique_edges: ', edge_list
+
+    #>>> per_player_goal_dict[k[0]]
+    # {'playerName': 'Cuellar, Carlos', 'playerPosition': 'Defender', 'team_id': 56, 'fail_goals': 0, 'success_goals': 2} 
 
 
 
